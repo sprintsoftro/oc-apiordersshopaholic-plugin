b685100f692da90220ff8b93e011d345b0e3b186
Alvaro Canepa <bfpdevel@gmail.com>
2021-05-31 09:00:16 -0300

    - Add status routes

--- routes/orders.php
+++ routes/orders.php
@@ -6,6 +6,21 @@ Route::prefix('orders')
             Route::post('create', 'Orders@create')->name('create');
             Route::get('{id}/position', 'Orders@positions')->name('position');
             Route::any('ipn', 'Orders@ipn')->name('ipn');
+            Route::apiResource('status', 'Statuses', ['only' => ['index', 'show']]);
+
+            if (has_jwtauth_plugin()) {
+                Route::apiResource(
+                    'status',
+                    'Statuses',
+                    [
+                        'only' => [
+                            'store',
+                            'update',
+                            'destroy'
+                        ]
+                    ]
+                );
+            }
         }
     );
 
